8a3fe7c0f443b1d9cf55b3218974be7d1d07ca14
bodjernisschroder <bdsc63266@edu.ucl.dk>
2025-03-18 11:40:51 +0100

    hej

---
+++ Client/Controllers/ChatController.cs
@@ -0,0 +1,26 @@
+﻿using BlazorStaticWebApps.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorStaticWebApps.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ChatController : ControllerBase
+    {
+        private static List<ChatMessage> messages = new List<ChatMessage>();
+
+        [HttpGet]
+        public IEnumerable<ChatMessage> GetMessages()
+        {
+            return messages.OrderBy(m => m.Timestamp);
+        }
+
+        [HttpPost]
+        public IActionResult PostMessage(ChatMessage message)
+        {
+            message.Timestamp = DateTime.Now;
+            messages.Add(message);
+            return Ok();
+        }
+    }
+}
